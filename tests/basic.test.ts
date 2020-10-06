a13c9f41d1a399c223ea018dfaf362558d4617ba
SURESHBA <SURESHBA@SURESHBA-IN.idc.oracle.com>
2020-10-06 17:03:56 +0530

    fixes for expect

--- tests/basic.test.ts
+++ tests/basic.test.ts
@@ -1,4 +1,5 @@
-import { browser} from 'protractor'
+import {browser, element, by, By, $, $$, ExpectedConditions} from 'protractor';
+
 describe('Protractor Typescript Demo', function() {
 	it('title verifications', function() {
 		console.log("Running first test case....");
@@ -8,4 +9,15 @@ describe('Protractor Typescript Demo', function() {
 			browser.sleep(5000);
 	  	})
 	});
-});
+}); 
+
+describe('async function', function() {
+	it('should wait on async function in conditional', async function() : Promise<any> {
+	  await browser.get('http://www.angularjs.org');
+	  let todoList = element.all(by.repeater('todo in todoList.todos'));
+	  if ((await todoList.count()) > 1) {
+		debugger
+		expect((await todoList.get(1).getText())).toEqual('build an AngularJS app');
+	  }
+	});
+  });
