f7b0981d56f194684e26275fbbb779a0faaddf4f
srikanth3git <63552765+srikanth3git@users.noreply.github.com>
2021-04-26 20:54:02 +0530

    change

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,7 +2,9 @@ exports.config = {
         framework: 'jasmine', //Type of Framework used 
         seleniumAddress: 'http://localhost:4444/wd/hub',
         //directConnect:true, 
-        specs: ['tests/**/*[tT]est.ts'], //Name of the Specfile 
+        specs: ['tests/**/*[tT]est.ts',
+      'tests/Manager.test.ts'], //Name of the Specfile 
+      
         onPrepare() { 
               require('ts-node').register({ 
               project: require('path').join(__dirname, './tsconfig.json') // Relative path of tsconfig.json file 
